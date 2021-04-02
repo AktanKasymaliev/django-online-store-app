964ab0c5dce999951a708d42649f88c3adcc25a8
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2021-04-02 18:25:15 +0600

    backend side is ready

--- api_side/models.py
+++ api_side/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+#TODO сделать каждой категории свой класс
+# Category/Product - все основные классы
+
---
+++ c_user/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from .views import (RegisterView, activating,
+                TokenObtainPairView, UsersView,
+                UserUpdateView)
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('register/', RegisterView.as_view()),
+    path('activate/<slug:uidb64>/<slug:token>/', activating, name='activate'),
+    path('auth/token/', TokenObtainPairView.as_view(), name='access'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('users/', UsersView.as_view(), name='users'),
+    path('user/upd/<int:pk>/', UserUpdateView.as_view(), name='user_update')
+]
